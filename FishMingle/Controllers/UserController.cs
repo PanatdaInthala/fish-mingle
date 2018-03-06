0e59fbab05fe6d7183576ac5e3368bcd1e39687d
tyler kostelak <tkostelak@gmail.com>
2018-03-06 11:43:55 -0800

    controller adds routes for views.

--- FishMingle/Controllers/UserController.cs
+++ FishMingle/Controllers/UserController.cs
@@ -8,11 +8,60 @@ namespace FishMingle.Controllers
 
   public class UserController : Controller
   {
-
+    //USER IS ABLE TO BROWSE OTHER PROFILES ON THIS PAGE
     [HttpGet("/users")]
-    public ActionResult Index()
+    public ActionResult Users()
     {
-      return View();
+      List<User> userList = User.GetAll();
+      return View(userList);
+    }
+
+    //FORM FOR CREATING USER
+    [HttpGet("/users/create")]
+    public ActionResult CreateUserForm()
+    {
+      return View("NewUserForm");
+    }
+
+    [HttpPost("/users")]
+    public ActionResult CreateUser()
+    {
+      string userNameActual = Request.Form["userName"];
+      //THE USERS SPECIES ID WILL BE INPUT HERE
+      string userNameProfile = Request.Form["userNameProfile"]);
+      string userPassword = Request.Form["userPassword"]);
+      User newUser = new User( userNameActual, speciesId, userNameProfile, userPassword );
+      newUser.Save();
+
+      return View("UserConfirmation", newUser);
+    }
+
+    [HttpGet("users/{id}/update/password")]
+    public ActionResult UpdatePasswordForm()
+    {
+      return View("UpdatePasswordForm");
+    }
+
+    [HttpPost("users/{id}/update/password")]
+    public ActionResult UpdatePassword()
+    {
+      User thisUser = User.Find(id);
+      thisUser.UpdatePassword(Request.Form["updatePassword"]);
+      return View ("UpdatePasswordConfirmation");
+    }
+
+    [HttpGet("users/{id}/update/username")]
+    public ActionResult UpdateUsername()
+    {
+      return View ("UpdateUserNameForm");
+    }
+
+    [HttpPost("users/{id}/update/userName")]
+    public ActionResult UpdateUsername()
+    {
+      User thisUser = User.Find(id);
+      thisUser.UpdateUser(Request.Form["UpdateUsername"]);
+      return View ("UpdateUsernameConfirmation");
     }
   }
 }
